3733b8d749a3a6997a3d9f5e0ff074b419ac2201
Cankkun <nguyenduccanh177@gmail.com>
2026-06-04 16:37:21 +0700

    feat: initialize AppModule with AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PublicController } from './public.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PublicController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
